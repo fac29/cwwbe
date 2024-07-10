b4b34115aefdf93b88478218ee2be560b70b1cdf
thatGui <thatguifonseca@gmail.com>
2024-07-10 12:14:02 +0100

    added more stuff to the interfaces

--- domain/Interfaces/IIngredient.cs
+++ domain/Interfaces/IIngredient.cs
@@ -6,5 +6,17 @@ namespace domain.entities
     {
         Guid Id { get; }
         string Name { get; }
+
+        // Method to retrieve all ingredients
+        IEnumerable<IIngredient> GetAllIngredients();
+
+        // Method to retrieve an ingredient by id
+        IIngredient GetIngredientById(Guid id);
+
+        // Method to add ingredients
+        void AddIngredient(IIngredient ingredient);
+
+        // Method to delete ingredients
+        void DeleteIngredient(Guid id);
     }
 }
